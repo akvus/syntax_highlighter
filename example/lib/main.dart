43890d6becbb81ae9896fbb0d9d3d36451751285
akvus <mr.akvus@gmail.com>
2021-10-12 15:43:37 +0200

    Update the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,7 +18,10 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
   final String title;
 
   @override
@@ -26,31 +29,95 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _exampleCode =
-      "class MyHomePage extends StatefulWidget { MyHomePage({Key key, this.title}) : super(key: key); final String title; @override _MyHomePageState createState() => _MyHomePageState();}";
+  String _exampleCode = '''class MyHomePage extends StatefulWidget {
+  MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+''';
 
   @override
   Widget build(BuildContext context) {
     final SyntaxHighlighterStyle style =
         Theme.of(context).brightness == Brightness.dark
-            ? SyntaxHighlighterStyle.darkThemeStyle()
-            : SyntaxHighlighterStyle.lightThemeStyle();
+            ? DarkSyntaxHighlighterStyle()
+            : LightSyntaxHighlighterStyle();
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: RichText(
-              text: TextSpan(
-                style: const TextStyle(fontFamily: 'monospace', fontSize: 10.0),
-                children: <TextSpan>[
-                  DartSyntaxHighlighter(style).format(_exampleCode),
-                ],
-              ),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: RichText(
+            text: TextSpan(
+              style: const TextStyle(fontFamily: 'monospace', fontSize: 10.0),
+              children: <TextSpan>[
+                DartSyntaxHighlighter(
+                  syntaxHighlighterStyle: style,
+                ).format(_exampleCode)
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
+}
+
+class LightSyntaxHighlighterStyle extends SyntaxHighlighterStyle {
+  @override
+  TextStyle? get baseStyle => const TextStyle(color: Color(0xFF000000));
+
+  @override
+  TextStyle? get classStyle => const TextStyle(color: Color(0xFF512DA8));
+
+  @override
+  TextStyle? get commentStyle => const TextStyle(color: Color(0xFF9E9E9E));
+
+  @override
+  TextStyle? get constantStyle => const TextStyle(color: Color(0xFF795548));
+
+  @override
+  TextStyle? get keywordStyle => const TextStyle(color: Color(0xFF9C27B0));
+
+  @override
+  TextStyle? get numberStyle => const TextStyle(color: Color(0xFF1565C0));
+
+  @override
+  TextStyle? get punctuationStyle => const TextStyle(color: Color(0xFF000000));
+
+  @override
+  TextStyle? get stringStyle => const TextStyle(color: Color(0xFF43A047));
+}
+
+class DarkSyntaxHighlighterStyle extends SyntaxHighlighterStyle {
+  @override
+  TextStyle? get baseStyle => const TextStyle(color: Color(0xFFFFFFFF));
+
+  @override
+  TextStyle? get classStyle => const TextStyle(color: Color(0xFF009688));
+
+  @override
+  TextStyle? get commentStyle => const TextStyle(color: Color(0xFF9E9E9E));
+
+  @override
+  TextStyle? get constantStyle => const TextStyle(color: Color(0xFF795548));
+
+  @override
+  TextStyle? get keywordStyle => const TextStyle(color: Color(0xFF80CBC4));
+
+  @override
+  TextStyle? get numberStyle => const TextStyle(color: Color(0xFF1565C0));
+
+  @override
+  TextStyle? get punctuationStyle => const TextStyle(color: Color(0xFFFFFFFF));
+
+  @override
+  TextStyle? get stringStyle => const TextStyle(color: Color(0xFF009688));
 }
